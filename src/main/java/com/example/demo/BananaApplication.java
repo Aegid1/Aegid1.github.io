ddfc7fb1da94e4ce161460dbc457ff799bf5a44c
Aegidius <Aegidius.Haslauer@Student.HTW-Berlin.de>
2023-04-27 23:37:13 +0200

    added postgresql dependency

--- src/main/java/com/example/demo/BananaApplication.java
+++ src/main/java/com/example/demo/BananaApplication.java
@@ -2,12 +2,36 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class BananaApplication {
 
+	private final String url = "jdbc:postgresql://localhost/dvdrental";
+	private final String user = "postgres";
+	private final String password = "<add your password>";
+
+
+	public Connection connect() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
 	public static void main(String[] args) {
+		
+		BananaApplication bananaPie = new BananaApplication();
+		bananaPie.connect();
 		SpringApplication.run(BananaApplication.class, args);
+
 	}
 
 }
