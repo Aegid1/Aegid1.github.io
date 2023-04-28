fc3c3cc8f0daf380b7f19fef0d47259fc9c7e6dd
Aegidius <Aegidius.Haslauer@Student.HTW-Berlin.de>
2023-04-28 19:15:21 +0200

    few changes to database and data-base-function

--- src/main/java/com/example/demo/BananaApplication.java
+++ src/main/java/com/example/demo/BananaApplication.java
@@ -9,26 +9,10 @@ import java.sql.SQLException;
 @SpringBootApplication
 public class BananaApplication {
 
-	private final String url = System.getenv("POSTGRESQL_URL");
-	private final String user = System.getenv("POSTGRESQL_USERNAME");
-	private final String password = System.getenv("POSTGRESQL_PASSWORD");
-
-	public Connection connect() {
-        Connection conn = null;
-        try {
-            conn = DriverManager.getConnection(url, user, password);
-            System.out.println("Connected to the PostgreSQL server successfully.");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-        return conn;
-    }
-
 
 	public static void main(String[] args) {
 		
-		BananaApplication bananaPie = new BananaApplication();
+		DBService bananaPie = new DBService();
 		bananaPie.connect();
 		SpringApplication.run(BananaApplication.class, args);
 
---
+++ src/main/java/com/example/demo/DBService.java
@@ -0,0 +1,85 @@
+package com.example.demo;
+
+import java.io.Serial;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class DBService {
+    
+    private final String url = System.getenv("POSTGRESQL_URL");
+	private final String user = System.getenv("POSTGRESQL_USERNAME");
+	private final String password = System.getenv("POSTGRESQL_PASSWORD");
+
+    @Autowired
+    private Connection conn;
+
+	public Connection connect() {
+    
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(url, user, password);
+            System.out.println("Connected to the PostgreSQL server successfully.");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return conn;
+    }
+
+
+    public void addUser(String firstname, String lastname, String password, String country, String city, String postalcode, String address, String email){
+
+        String sql = new StringBuffer()
+                    .append("INSERT INTO user_account (user_id, firstname, lastname, password, country, city, postalcode, address, email)")
+                    .append("VALUES (DEFAULT, ?, ?, ?, ?, ?, ?, ?, ?)")
+                    .toString();
+
+        try(PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ps.setString(1, firstname);
+            ps.setString(2, lastname);
+//eventuell hier noch abfragen ob passwort gut genug ist
+            ps.setString(3, password);
+//eventuell hier abfragen ob Land wirklich existiert
+            ps.setString(4, country);
+            ps.setString(5, city);
+            ps.setString(6, postalcode);
+            ps.setString(7, address);
+//eventuell hier noch abfragen ob gültige email -> enthält es ein "@"?, usw.
+            ps.setString(8, email);
+            ps.executeUpdate();
+        
+//Exception eventuell noch anpassen
+        } catch (SQLException e) {
+
+            System.out.println("Ausführung gescheitert!");
+
+        }          
+
+    }
+
+
+
+    public void deleteUser(int id){
+
+        String sql = ("DELETE FROM user_account WHERE user_id = ?");
+
+        try(PreparedStatement ps = conn.prepareStatement(sql)){
+
+            ps.setInt(1, id);
+            ps.executeUpdate();
+
+        } catch (SQLException e) {
+
+            System.out.println("Ausführung gescheitert!");
+
+        }  
+        
+    }
+
+}
